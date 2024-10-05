37db1a6a423b6b5e9ac1f23174dbdc0e60eda0b7
Svetlana <svetzayats@gmail.com>
2024-10-05 22:33:48 +0400

    :tada: Start astro project and add structure

---
+++ astro.config.mjs
@@ -0,0 +1,45 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: '🐰 Svetlana Zaiats',
+			social: {
+				github: 'https://github.com/Svetzayats',
+				linkedin: 'https://www.linkedin.com/in/svetlana-zayats/',
+				email: 'mailto:svetzayats@gmail.com',
+				telegram: 'https://t.me/SvZayats',
+				instagram: 'https://www.instagram.com/fare_dodger'
+			},
+			sidebar: [
+				{ label: 'About', link: '/about/' },
+				{
+					label: 'Dev Notes',
+					autogenerate: { directory: 'dev_notes' },
+				},
+				{
+					label: 'Dev Guides',
+					autogenerate: { directory: 'dev_guides' },
+				},
+				{
+					label: 'Content Reviews', 
+					autogenerate: { directory: 'content_reviews' },
+				},
+				{
+					label: 'Life Insights', 
+					autogenerate: { directory: 'life_insights' },
+				}, 
+				{
+					label: 'Travel & Places', 
+					autogenerate: { directory: 'travel_places' },
+				}, 
+				{
+					label: 'Occasional Daily',
+					autogenerate: { directory: 'occasional_daily' },
+				}
+			],
+		}),
+	],
+});
