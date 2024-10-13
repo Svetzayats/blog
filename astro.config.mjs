ed728e5d1896ab53d50205ca76e295540a1bb8d2
Svetlana <svetzayats@gmail.com>
2024-10-13 13:28:17 +0400

    :hammer: Making changes for publishing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://svetzayats.github.io/',
+  base: '/blog',
 	integrations: [
 		starlight({
 			title: '🐰 Svetlana Zaiats',
