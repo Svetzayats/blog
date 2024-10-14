bb573b919fead381e9e977faf0ea5935a927b359
Svetlana <svetzayats@gmail.com>
2024-10-14 18:32:06 +0400

    :globe_with_meridians: Add common info component with i18n

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,17 @@
-import { defineCollection } from 'astro:content';
-import { docsSchema } from '@astrojs/starlight/schema';
+import { defineCollection, z } from 'astro:content';
+import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
 	docs: defineCollection({ schema: docsSchema() }),
+	i18n: defineCollection({ type: 'data', schema: i18nSchema({
+		extend: z.object({
+			'something': z.string().optional(),
+			'bookInfo.title': z.string().optional(),
+			'bookInfo.author': z.string().optional(),
+			'bookInfo.started': z.string().optional(),
+			'bookInfo.finished': z.string().optional(),
+			'bookInfo.whyStarted': z.string().optional(),
+			'bookInfo.recommendations': z.string().optional(),
+		}),
+	}) }),
 };
