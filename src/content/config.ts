8d94f649cdc3716fbe8591d9642ca3df06c9102a
Svetlana <svetzayats@gmail.com>
2024-10-27 22:19:15 +0400

    :sparkles: Add ContentList and collecting read books

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,9 +2,16 @@ import { defineCollection, z } from 'astro:content';
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 import { blogSchema } from 'starlight-blog/schema';
 
+const customFields = z.object({
+	// for quering entries in collections
+	category: z.string().optional(),
+})
 export const collections = {
 	docs: defineCollection({ schema: docsSchema({
-		extend: (context) => blogSchema(context)
+		extend: (context) => {
+			const baseSchema = blogSchema(context);
+			return baseSchema.merge(customFields);
+		}
 	}) }),
 	i18n: defineCollection({ type: 'data', schema: i18nSchema({
 		extend: z.object({
