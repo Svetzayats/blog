efcd4b1d28508fc8c9229c5ce9dac26c6a8e9fba
Svetlana <svetzayats@gmail.com>
2025-04-13 11:53:27 +0400

    :note: Add note about CursorAI

--- .astro/astro/content.d.ts
+++ .astro/astro/content.d.ts
@@ -249,6 +249,13 @@ declare module 'astro:content' {
   collection: "docs";
   data: InferEntrySchema<"docs">
 } & { render(): Render[".mdx"] };
+"blog/2025_04_10.mdx": {
+	id: "blog/2025_04_10.mdx";
+  slug: "blog/2025_04_10";
+  body: string;
+  collection: "docs";
+  data: InferEntrySchema<"docs">
+} & { render(): Render[".mdx"] };
 "content_list/index.mdx": {
 	id: "content_list/index.mdx";
   slug: "content_list";
