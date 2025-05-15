cc4581a2ee84639245809b1d6257e48b78f43fc9
Svetlana <svetzayats@gmail.com>
2025-05-15 20:12:35 +0400

    :memo: Add post about JWT

--- .astro/astro/content.d.ts
+++ .astro/astro/content.d.ts
@@ -256,6 +256,13 @@ declare module 'astro:content' {
   collection: "docs";
   data: InferEntrySchema<"docs">
 } & { render(): Render[".mdx"] };
+"blog/2025_05_15.mdx": {
+	id: "blog/2025_05_15.mdx";
+  slug: "blog/2025_05_15";
+  body: string;
+  collection: "docs";
+  data: InferEntrySchema<"docs">
+} & { render(): Render[".mdx"] };
 "content_list/index.mdx": {
 	id: "content_list/index.mdx";
   slug: "content_list";
